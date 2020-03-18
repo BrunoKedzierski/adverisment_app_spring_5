8d58a8933eb76d02d003789f2731cc2596b7b7af
Bruno <bruno.kedzierski@wp.pl>
2020-03-18 15:44:18 +0100

    Initializing project

---
+++ src/main/java/com/bkk/adverisment_app_spring_5/AdverismentAppSpring5Application.java
@@ -0,0 +1,13 @@
+package com.bkk.adverisment_app_spring_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdverismentAppSpring5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdverismentAppSpring5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bkk/adverisment_app_spring_5/AdverismentAppSpring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkk.adverisment_app_spring_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdverismentAppSpring5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
