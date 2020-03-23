f32735a066d23bf6484ba387222f2b3237e97501
Bruno <bruno.kedzierski@wp.pl>
2020-03-23 15:45:44 +0100

    Creating the homepage and home controller

---
+++ advertsiment_web/src/main/java/com/bkk/advertisment_app_spring_5/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.bkk.advertisment_app_spring_5.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping({"","/"})
+    public String index(){
+        return "index";
+    }
+}
